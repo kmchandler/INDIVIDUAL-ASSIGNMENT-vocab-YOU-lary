3394e95d5a1e0879901d5b61a2f9b833f1a768dc
Kristen Chandler <kristenchandler@Kristens-MBP.attlocal.net>
2022-06-09 16:00:19 -0500

    finished readMe and completed all main goals for app

--- src/scripts/components/loginButton.js
+++ src/scripts/components/loginButton.js
@@ -2,7 +2,7 @@ import signIn from '../helpers/signIn';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<h5 id="welcomeMessage">Welcome to vocab-YOU-lary</h5><h2 id="welcomeMessage2">Login to view and manage your vocab cards.</h2><button id="google-auth" class="btn btn-danger">LOGIN WITH GOOGLE</button>';
   document.querySelector('#login-form-container').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
